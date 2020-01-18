b5956d8e4db8d47620083bcf70ec95ebc894d3f5
NadavTraub <nadavtraub@outlook.co.il>
2020-01-18 15:48:58 +0200

    feat: add test and connection controller

---
+++ src/main/java/mongo/controllers/ConnectionController.java
@@ -0,0 +1,15 @@
+package mongo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConnectionController {
+
+    @GetMapping(path = "/live")
+    public ResponseEntity<String> live() {
+        return new ResponseEntity<>("i'm live now :)", HttpStatus.OK);
+    }
+}
